86239d17464522a76c484dfd40ebd347df6aca1e
Elisama Talita Cabral Santana <82914088+TalitaCbral@users.noreply.github.com>
2021-12-29 18:26:59 -0300

    First Commit

--- upload-frontend-react-hooks/src/App.tsx
+++ upload-frontend-react-hooks/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+ 
+import GlobalStyle from "./styles/global";
+import { Container, Content } from "./styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Upload from "./components/Upload";
+/* import FileList from "./components/FileList";
+
+import { FileProvider } from "./context/files"; */
+  const App: React.FC = () => (
+   /*  <FileProvider> */
+      <Container>
+        <Content>
+          <Upload />
+         {/*  <FileList /> */}
+        </Content>
+        <GlobalStyle />
+      </Container>
+ /*    </FileProvider> */
+  )
+
 
 export default App;

c34f621e644bb7e4751ca91f104b486438181052
rafapearl <rafaironm2@gmail.com>
2021-12-30 15:23:15 -0300

    pdf

--- upload-frontend-react-hooks/src/components/Upload/index.tsx
+++ upload-frontend-react-hooks/src/components/Upload/index.tsx
@@ -5,47 +5,49 @@ import { useFiles } from "../../context/files";
 import { DropContainer, UploadMessage } from "./style";
 
 function Upload() {
-const { handleUpload } = useFiles();
+  const { handleUpload } = useFiles();
 
-const onDrop = useCallback(
+  const onDrop = useCallback(
     (files) => {
-        handleUpload(files);
-},
+      handleUpload(files);
+    },
     [handleUpload]
-);
+  );
 
-const {
-    getRootProps,
-    getInputProps,
-        isDragActive,
-    isDragReject,
-    } = useDropzone({
-        accept: ["image/jpeg", "image/pjpeg", "image/png", "image/gif"],
-        onDrop,
-});
+  const { getRootProps, getInputProps, isDragActive, isDragReject } =
+    useDropzone({
+      accept: [
+        "application/pdf",
+        "image/jpeg",
+        "image/pjpeg",
+        "image/png",
+        "image/gif",
+      ],
+      onDrop,
+    });
 
-const renderDragMessage = useCallback(() => {
+  const renderDragMessage = useCallback(() => {
     if (!isDragActive) {
-    return <UploadMessage>Arraste imagens aqui...</UploadMessage>;
+      return <UploadMessage>Arraste imagens aqui...</UploadMessage>;
     }
 
-if (isDragReject) {
-    return (
+    if (isDragReject) {
+      return (
         <UploadMessage type="error">
-        Tipo de arquivo não suportado
+          Tipo de arquivo não suportado
         </UploadMessage>
-    );
+      );
     }
 
     return <UploadMessage type="success">Solte as imagens aqui</UploadMessage>;
-}, [isDragActive, isDragReject]);
- 
-return (
-<DropContainer {...getRootProps()}>
-    <input {...getInputProps()} />
-    {renderDragMessage()}
+  }, [isDragActive, isDragReject]);
+
+  return (
+    <DropContainer {...getRootProps()}>
+      <input {...getInputProps()} />
+      {renderDragMessage()}
     </DropContainer>
-);
+  );
 }
- 
+
 export default Upload;
